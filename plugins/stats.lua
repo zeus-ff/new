02da1dd750cf9ddbea42ddfc5fc0e3f1698c5f51
zeus-ff <tofaniyam@gmail.com>
2016-07-14 12:02:25 +0100

    Update and rename blackPlus.lua to stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'black' then -- Put everything you like :)
+  if matches[1]:lower() == 'tesla' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /black ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /tesla ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "black" then -- Put everything you like :)
+    if matches[2] == "tesla" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,7 +143,7 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (black)",
+    "^[#!/]([Ss]tats) (tesla)",
 	"^[#!/]([Bb]lack)"
     }, 
   run = run
